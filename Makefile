cf76d0090390a4a661a92cffd9fe687e62c60558
Dmitry Vekshin <vekshin8@gmail.com>
2022-09-06 19:52:15 +0300

    makefile and first cli static method

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+run-dist:
+	java -jar build/install/app/lib/app-1.jar
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,14 @@
 package hexlet.code;
 
 
+import java.util.Scanner;
+
+import static hexlet.code.Cli.askName;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        askName();
     }
 }
